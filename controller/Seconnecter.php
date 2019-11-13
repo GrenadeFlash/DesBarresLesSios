855c36e8d165bfb3c9ade6cceb481f67d13470dc
GrenadeFlash <56717723+GrenadeFlash@users.noreply.github.com>
2019-11-13 15:03:31 +0100

    function commander update

--- controller/Seconnecter.php
+++ controller/Seconnecter.php
@@ -48,7 +48,47 @@ class Seconnecter extends Controller
         header('Location:'.WEBROOT.'seconnecter'); 
     }
     function commander(){
-       
+       //L'appel de cette commande se fait seulement si des blagues ont déjà été commander donc pas besoin
+		// de faire une verification dessus
+        $test="(";
+		foreach($_SESSION['panier'] as $ligne => $value){
+			$test.=$ligne.",";
+		}
+		$test.="0)";
+		$blague = Model::load("blagues");
+		$blagueRes = array("produit"=>$blague->find(Model::connexion(), array('condition'=>"id_blague in $test")));
+		$variables['cmde']=array('mescommandes'=>$blagueRes['produit']);
+		$this->set($variables);
+		Model::deconnexion();
+		$today = date("Y-m-d");
+		$idA = $_SESSION['auteur'][0]->id_auteur;
+		$total = 0;
+		foreach($blagueRes['produit'] as $ligne => $prop){
+			$total = $total + $prop -> px_blague;
+		}
+		$data= array(
+			"id_auteur"=>$idA,
+			"dated_commande"=>$today,
+			"tot_commande"=>$total
+		);
+		$tabCom = Model::load("commande");
+		$tabCom -> save(Model::connexion(),$data);
+		
+		$lcommandeRes = array("produit"=>$tabCom->find(Model::connexion(), array('condition'=>"id_auteur = $idA && dated_commande = '$today' && tot_commande = $total")));
+		Model::deconnexion();
+		
+		$tabLCom = Model::load("lcommande");
+		foreach($blagueRes['produit'] as $ligne => $prop) {
+			$data2 = array(
+				"id_commande"=>$lcommandeRes['produit'][0]->id_commande,
+				"id_blague"=>$prop -> id_blague
+			);
+			$tabLCom -> save(Model::connexion(),$data2);
+		}
+		Model::deconnexion();
+		$_SESSION['panier'] = null;
+		
+        $this->render('commander');
     }
     
     function commanderfinaliser(){
